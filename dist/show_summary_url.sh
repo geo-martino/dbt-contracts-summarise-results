2b2030ebe5091f204363ca920f3c1fda47b9e239
geo-martino <gm.login+tech@pm.me>
2024-11-06 18:03:08 -0500

    add job name arg

--- dist/show_summary_url.sh
+++ dist/show_summary_url.sh
@@ -4,12 +4,12 @@ run_url_path="$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
 # TODO: workaround to get job ID from API. Fix hardcoded name in 'contains' jq filter.
 job_url="$GITHUB_API_URL/repos/${run_url_path}/jobs"
-job_response="$(curl -X GET \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
+job_response="$(curl -s -X GET -f \
+    -H "Authorization: Bearer $GH_TOKEN" \
     -H "Accept: application/vnd.github+json" \
     --url "$job_url" \
 )"
-job_id="$(jq -r ".jobs[] | select(.name | ascii_downcase | contains(\"${GITHUB_JOB,,}\")) | .id" <<< "$job_response")"
+job_id="$(jq -r "[.jobs[] | select(.name | ascii_downcase | contains(\"${GH_JOB,,}\"))][0].id" <<< "$job_response")"
 
 if [[ "$GITHUB_EVENT_NAME" =~ "pull_request" ]]; then
   run_url_path="${run_url_path}?pr=$GH_EVENT_NUMBER"
